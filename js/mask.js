ce84ff7b820e83664d2ace1e27dc10ee67bfb6af
tamiresslelis <tamiresslelis@gmail.com>
2018-01-04 11:56:57 -0200

    fist

---
+++ js/mask.js
@@ -0,0 +1,29 @@
+function mascara() {
+
+    $('#currency').maskMoney();
+
+}
+function formato(){
+var test= document.getElementById("currency").value;
+
+
+function getMoney( str )
+{
+        return parseInt( str.replace(/[\D]+/g,'') );
+}
+function formatReal( int )
+{
+        var tmp = int+'';
+        tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+        if( tmp.length > 6 )
+                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+
+        return tmp;
+}
+
+
+var int = getMoney( test );
+//alert( int );
+
+console.log( formatReal( int ) );
+}
